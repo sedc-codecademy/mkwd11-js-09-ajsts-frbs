a5f0846c3d45dd19350baabed57042f2bce79305
Ivo Kostovski <ivo.kostovski@gmail.com>
2023-09-05 18:34:42 +0200

    Add auth component

--- G10/class12/src/app/components/auth/auth.component.ts
+++ G10/class12/src/app/components/auth/auth.component.ts
@@ -5,4 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css'],
 })
-export class AuthComponent {}
+export class AuthComponent {
+  hidePassword: boolean = true;
+  showLoginForm: boolean = true;
+}
--- G10/class12/src/app/material/material.module.ts
+++ G10/class12/src/app/material/material.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+const modules = [
+  MatInputModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatButtonModule,
+];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatInputModule, MatFormFieldModule],
-  exports: [MatInputModule, MatFormFieldModule],
+  imports: [...modules],
+  exports: [...modules],
 })
 export class MaterialModule {}
